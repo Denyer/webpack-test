e7ff0d26915fe5b98e625396d6e43f25d3358a96
Denyer <mtdenyer@gmail.com>
2018-03-02 14:41:09 +0000

    Add loading and compiling scss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import $ from "jquery"
+import scss from './scss/main.scss';
 
 $('#replace').text('This has been replace by jQuery');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js'
+  },
+  watch: true,
+  devServer: {
+    inline: true
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
